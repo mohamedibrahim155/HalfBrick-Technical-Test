8349916e5f5ceff853624294d359da35280d2cc9
mohamedibrahim155 <63718196+mohamedibrahim155@users.noreply.github.com>
2024-10-23 12:34:06 -0400

    Added Bouncy element in pressure pad

--- Assets/Scripts/PressurePad.cs
+++ Assets/Scripts/PressurePad.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,8 +7,9 @@ public class PressurePad : MonoBehaviour {
 
     private SpriteRenderer m_sprite = null;
     private Color m_defaultColor = new Color();
-	// Use this for initialization
-	void Start () {
+    [SerializeField] private float m_forcePush = 5;
+    // Use this for initialization
+    void Start () {
         m_sprite = transform.GetComponent<SpriteRenderer>();
         m_defaultColor = m_sprite.color;
     }
@@ -20,10 +22,30 @@ public class PressurePad : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         m_sprite.color = new Color(m_defaultColor.r / 2, m_defaultColor.g / 2, m_defaultColor.b / 2, 1);
+
+        ProcessCollision(collision);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         m_sprite.color = m_defaultColor;
+    }
+
+    private void ProcessCollision(Collider2D collision)
+    {
+        // Unity Player layer
+        const int playerMask = 1 << 6;
+
+        // Collision with player
+        if ((playerMask & (1 << collision.gameObject.layer)) != 0)
+        {
+            Player player = collision.gameObject.GetComponent<Player>();
+
+            if (player != null)
+            {
+                // adding additional force based on jump pad
+               player.AddAdditionalJumpForce(m_forcePush);
+            }
+        }
     }
 }
